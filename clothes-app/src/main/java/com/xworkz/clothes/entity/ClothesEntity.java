bad1513722795a014a475517458ccdc6ba83023c
rohannaik8 <98726821+rohannaik8@users.noreply.github.com>
2022-05-01 22:38:21 +0530

    helo

---
+++ clothes-app/src/main/java/com/xworkz/clothes/entity/ClothesEntity.java
@@ -0,0 +1,24 @@
+package com.xworkz.clothes.entity;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="clothes")
+public class ClothesEntity {
+	@Id
+	@Column(name="clothes_id")
+	private int id;
+	private String brand;
+	private double price;
+	private Date year;
+	
+}
+
+
